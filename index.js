b4a11b5541cc023786393e7cc08a04a6f453bc11
Prathmesh <148199267+prath47@users.noreply.github.com>
2024-03-16 23:22:55 +0530

    Update index.js
    mongodb atalas connected

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(checkForAuthenticationCookie("token"));
 app.use(express.urlencoded({ extended: false }));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/qr-generator")
+  .connect(`mongodb://shortstime400:${process.env.mongoPassword}@ac-7lrmuae-shard-00-00.gxkdmzl.mongodb.net:27017,ac-7lrmuae-shard-00-01.gxkdmzl.mongodb.net:27017,ac-7lrmuae-shard-00-02.gxkdmzl.mongodb.net:27017/?ssl=true&replicaSet=atlas-kmbelt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=QR-Code-Generator`)
   .then((e) => console.log("connected to mongodb"))
   .catch((error) => {
     console.log(error);
